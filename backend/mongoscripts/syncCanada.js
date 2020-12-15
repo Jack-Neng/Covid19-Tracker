d303b2d2ff81708634a03c60d2aefbb40484ef45
Jack Neng <jackwneng@gmail.com>
2020-12-14 19:11:54 -0800

    change update to bulkwrite to increase performance

--- backend/mongoscripts/syncCanada.js
+++ backend/mongoscripts/syncCanada.js
@@ -2,10 +2,11 @@ const CronJob = require("cron").CronJob;
 const CanadaCases = require("../models/canada");
 const { downloadFile, deleteFile, readFile } = require("./util");
 
-const debug = "59 23 * * *";
+const debug = "05 19 * * *";
+const everyHour = "0 * * * *";
 
 // db.canadacases.createIndex({ prname: 1, date: 1}, { unique: true })
-module.exports = new CronJob(debug, async () => {
+module.exports = new CronJob(everyHour, async () => {
   const path = "./assets/covid19-download.csv";
   const url =
     "https://health-infobase.canada.ca/src/data/covidLive/covid19-download.csv";
@@ -14,33 +15,51 @@ module.exports = new CronJob(debug, async () => {
   var filename = await downloadFile(path, url);
   var filedata = await readFile(filename);
 
-  // filter old data
-  var data = await CanadaCases.find({});
-  var dateMatch = data.map((a) => a.date.toLocaleDateString());
-  var newData = filedata.filter(
-    (d) => !dateMatch.includes(d.date.toLocaleDateString())
-  );
+  // // filter old data
+  // var data = await CanadaCases.find({});
+  // var dateMatch = data.map((a) => a.date.toLocaleDateString());
+  // var newData = filedata.filter(
+  //   (d) => !dateMatch.includes(d.date.toLocaleDateString())
+  // );
   console.log("# of data: ", filedata.length);
-  console.log("# of Old data: ", data.length);
-  console.log("# of New data: ", newData.length);
+  // console.log("# of Old data: ", data.length);
+  // console.log("# of New data: ", newData.length);
+
+  var queryArr = filedata.map((data) => ({
+    updateOne: {
+      filter: { prname: data.prname, date: data.date },
+      update: data,
+      upsert: true,
+    },
+  }));
 
   // update database
-  var i = 1;
-  for (const data of filedata) {
-    await CanadaCases.updateOne(
-      { prname: data.prname, date: data.date },
-      data,
-      { upsert: true, new: true },
-      (err, doc) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(`Updated ${i} of ${filedata.length} data.`);
-        }
-      }
-    );
-    i += 1;
-  }
+  await CanadaCases.bulkWrite(queryArr)
+    .then((result) => {
+      console.log("Update successfully");
+      // console.log(JSON.stringify(result, null, 2));
+    })
+    .catch((err) => {
+      console.log("Update error");
+      console.log(JSON.stringify(err, null, 2));
+    });
+
+  // var i = 1;
+  // for (const data of filedata) {
+  //   await CanadaCases.updateOne(
+  //     { prname: data.prname, date: data.date },
+  //     data,
+  //     { upsert: true, new: true },
+  //     (err, doc) => {
+  //       if (err) {
+  //         console.log(err);
+  //       } else {
+  //         console.log(`Updated ${i} of ${filedata.length} data.`);
+  //       }
+  //     }
+  //   );
+  //   i += 1;
+  // }
 
   deleteFile(filename);
 });
--- backend/mongoscripts/syncGlobal.js
+++ backend/mongoscripts/syncGlobal.js
@@ -2,10 +2,11 @@ const CronJob = require("cron").CronJob;
 const GlobalCases = require("../models/global");
 const { downloadFile, deleteFile, readFile } = require("./util");
 
-const debug = "55  20 * * *";
+const debug = "23 18 * * *";
+const everyHour = "30 * * * *";
 
 // db.globalcases.createIndex({ Country: 1, Date_reported: 1}, { unique: true })
-module.exports = new CronJob(debug, async () => {
+module.exports = new CronJob(everyHour, async () => {
   const path = "./assets/WHO-COVID-19-global-data.csv";
   const url = "https://covid19.who.int/WHO-COVID-19-global-data.csv";
 
@@ -14,34 +15,33 @@ module.exports = new CronJob(debug, async () => {
   var filename = await downloadFile(path, url);
   var filedata = await readFile(filename);
 
-  // filter old data
-  var data = await GlobalCases.find({});
-  var dateMatch = data.map((a) => a.Date_reported.toLocaleDateString());
-  var newData = filedata;
-  //   var newData = filedata.filter(
-  //     (d) => !dateMatch.includes(d.Date_reported.toLocaleDateString())
+  // // filter old data
+  // var data = await GlobalCases.find({});
+  // var dateMatch = data.map((a) => a.Date_reported.toLocaleDateString());
+  // var newData = filedata.filter(
+  //  (d) => !dateMatch.includes(d.Date_reported.toLocaleDateString())
   // );
   console.log("# of data: ", filedata.length);
-  console.log("# of Old data: ", data.length);
-  console.log("# of New data: ", newData.length);
+  // console.log("# of Old data: ", data.length);
+  // console.log("# of New data: ", newData.length);
 
   // update new data to mongo
-  var i = 1;
-  for (const data of newData) {
-    await GlobalCases.updateOne(
-      { Country: data.Country, Date_reported: data.Date_reported },
-      data,
-      { upsert: true, new: true },
-      (err, doc) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(`Updated ${i} of ${newData.length} data.`);
-        }
-      }
-    );
-    i += 1;
-  }
+  var queryArr = filedata.map((data) => ({
+    updateOne: {
+      filter: { Country: data.Country, Date_reported: data.Date_reported },
+      update: data,
+      upsert: true,
+    },
+  }));
+  await GlobalCases.bulkWrite(queryArr)
+    .then((result) => {
+      console.log("Update successfully");
+      // console.log(JSON.stringify(result, null, 2));
+    })
+    .catch((err) => {
+      console.log("Update error");
+      console.log(JSON.stringify(err, null, 2));
+    });
 
   // delete file
   deleteFile(filename);
